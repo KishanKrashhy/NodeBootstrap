ca060a24b100a07bf86157fe1938cf23160ade2c
KishanKrashhy <kishan.krishna.patel44@gmail.com>
2017-05-27 05:09:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+An Example of designing web app using Nodejs, ExpressJS, Bootstrap and Angular. The project is still in developement stage
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+
+var router = express.Router();
+var path = __dirname+'/views/';
+
+router.use((req,res,next)=>{
+    console.log('/'+req.method)
+    next()
+})
+
+router.get('/',(req,res)=>{
+    res.sendFile(path+'index.html');
+})
+
+router.get('/about',(req,res)=>{
+    res.sendFile(path+'about.html');
+})
+
+router.get('/contact',(req,res)=>{
+    res.sendFile(path+'contact.html');
+})
+
+app.use('/',router);
+
+router.get('*',(req,res)=>{
+    res.sendFile(path+'404.html');
+})
+
+app.listen(3000,()=>{
+    console.log('listening at Port 3000')
+})
